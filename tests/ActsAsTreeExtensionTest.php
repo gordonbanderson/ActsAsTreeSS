e28967af7d126cc77c6aefa5f21e18122c83a42e
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-30 22:44:30 +0700

    WIP: Padding out tests

--- tests/ActsAsTreeExtensionTest.php
+++ tests/ActsAsTreeExtensionTest.php
@@ -1,6 +1,15 @@
 <?php
 
 class ActsAsTreeExtensionTest extends SapphireTest {
+
+    public static $fixture_file = "acts_as_tree/tests/acts_as_tree.yml";
+
+    public function setUp() {
+        parent::setUp();
+        $page = $this->objFromFixture('Page', 'homepage');
+        $page->requireDefaultRecords();
+    }
+
 	public function testOnBeforeWrite() {
 		$this->markTestSkipped('TODO');
 	}
