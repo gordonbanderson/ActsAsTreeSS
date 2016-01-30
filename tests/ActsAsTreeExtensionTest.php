4f259496cb986bc1e89ed23de25c8716edebe254
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-30 07:42:24 +0700

    WIP: Initial tests shell

---
+++ tests/ActsAsTreeExtensionTest.php
@@ -0,0 +1,20 @@
+<?php
+
+class ActsAsTreeExtensionTest extends SapphireTest {
+	public function testOnBeforeWrite() {
+		$this->markTestSkipped('TODO');
+	}
+
+	public function testOnAfterWrite() {
+		$this->markTestSkipped('TODO');
+	}
+
+	public function testPaddedNumber() {
+		$this->markTestSkipped('TODO');
+	}
+
+	public function testRequireDefaultRecords() {
+		$this->markTestSkipped('TODO');
+	}
+
+}
