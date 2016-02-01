e96fe8c70583c1640670e21b77888acdf7e06d01
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-02-01 11:38:34 +0700

    WIP: Debugging

--- tests/ActsAsTreeExtensionTest.php
+++ tests/ActsAsTreeExtensionTest.php
@@ -11,15 +11,20 @@ class ActsAsTreeExtensionTest extends SapphireTest {
     }
 
     public function testRequireDefaultRecords() {
+        error_log("\n\n\n\n\n\n");
         error_log('-------------');
         error_log('Testing RDFR');
 
         //Reset Depth and Lineage as if they had just been created
-        $sql = 'UPDATE "SiteTree" SET Lineage=null, Depth=0';
+        $sql = 'DELETE FROM SiteTree';
+        DB::query($sql);
+        $sql = 'DELETE FROM SiteTree_Live';
         DB::query($sql);
         $page = new SiteTree();
         $page->requireDefaultRecords();
         foreach (Page::get() as $page) {
+            error_log('QQ: ' . $page->LineageState);
+
             $this->checkPage($page);
         }
     }
@@ -32,16 +37,28 @@ class ActsAsTreeExtensionTest extends SapphireTest {
 	}
 
     public function testCreateWithAncestry() {
+        error_log("\n\n\n\n\n\n");
+        error_log('+++++++++++++++++++++++++++++++');
         $parent = $this->objFromFixture('Page', 'menu040401');
+        error_log('PARENT LINEAGE: ' . $parent->Lineage);
         $page = new Page();
         $page->Title = 'Menu 04040101';
-        $page->ParentID = $page->ID;
+        $page->ParentID = $parent->ID;
+        error_log('SET PARENT ID TO ' . $page->ParentID);
         $page->write();
         $this->checkPage($page);
     }
 
 	public function testEdit() {
+        error_log("\n\n\n\n********** TEST CAN EDIT ***********\n\n\n\n");
+        foreach (Page::get() as $page) {
+            error_log($page->Title. ', ' . $page->ID . ', ParentID ' . $page->ParentID . ', D/L = ' .$page->Depth .'/' . $page->Lineage);
+        }
+
         $page = $this->objFromFixture('Page', 'menu040401');
+        error_log('EDIT: Original lineage:' . $page->Lineage);
+        $this->checkPage($page);
+
         $originalDepth = $page->Depth;
         $originalLineage = $page->Lineage;
         $page->Title = 'A different title';
@@ -55,18 +72,23 @@ class ActsAsTreeExtensionTest extends SapphireTest {
     /*
     Child pages need updated as well
      */
-    public function testMove() {
+    public function testMoveDown() {
         $page = $this->objFromFixture('Page', 'toplevel04');
+        error_log('==== MOVING ====');
+        error_log('MOVING PAGE ' . $page->ID .', ' . $page->Title);
 
-        foreach (Page::get() as $page) {
-            error_log('BEFORE MOVE: PAGE ' . $page->ID . ': ' . $page->Lineage);
+        foreach (Page::get() as $pageCheck) {
+            error_log('BEFORE MOVE: PAGE ' . $pageCheck->ID . ': ' . $pageCheck->Lineage);
         }
 
         $newParent = $this->objFromFixture('Page', 'toplevel01');
+        error_log('SETTING PARENT ID ' . $newParent->ID);
         $page->ParentID = $newParent->ID;
+
+        error_log("REWRITING PAGE " . $page.', ' . $page->Title);
         $page->write();
-        foreach (Page::get() as $page) {
-            error_log('AFTER MOVE: PAGE ' . $page->ID . ': ' . $page->Lineage);
+        foreach (Page::get() as $pageCheck) {
+            error_log('AFTER MOVE: PAGE ' . $pageCheck->ID . ': ' . $pageCheck->Lineage);
         }
     }
 
@@ -90,11 +112,13 @@ class ActsAsTreeExtensionTest extends SapphireTest {
             $lineage .= str_pad($id, 5, '0', STR_PAD_LEFT);
         }
 
+error_log('Checking depth ' . $page->Depth);
+        error_log('Checking lineage ' . $page->Lineage);
+        error_log(print_r($lineageIDs,1));
+
         $this->assertEquals($depth, $page->Depth);
         $this->assertEquals($lineage, $page->Lineage);
 
-        error_log('Checking depth ' . $page->Depth);
-        error_log('Checking lineage ' . $page->Lineage);
 
     }
 
